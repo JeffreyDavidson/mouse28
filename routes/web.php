fa858a54ce42f276aba80a186134d1f9ec5b97a4
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2026-02-24 03:06:23 +0000

    feat: add contact page, routes, seeders with 8 blog posts and 4 episodes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\EpisodeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/episodes', [EpisodeController::class, 'index']);
-Route::get('/episodes/{episode:slug}', [EpisodeController::class, 'show']);
 Route::get('/blog', [PostController::class, 'index']);
 Route::get('/blog/{post:slug}', [PostController::class, 'show']);
+Route::get('/episodes', [EpisodeController::class, 'index']);
+Route::get('/episodes/{episode:slug}', [EpisodeController::class, 'show']);
 Route::get('/about', fn () => view('about'));
+Route::get('/contact', [ContactController::class, 'show']);
+Route::post('/contact', [ContactController::class, 'store']);
