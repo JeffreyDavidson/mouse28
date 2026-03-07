0d0f8992dc1fae3f0f06150e967b7e18f8c0fcb8
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-07 23:12:08 +0000

    feat: branded headers for Posts and Episodes list pages

--- app/Filament/Resources/Posts/Pages/ListPosts.php
+++ app/Filament/Resources/Posts/Pages/ListPosts.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\Posts\Pages;
 
 use App\Filament\Resources\Posts\PostResource;
+use App\Models\Post;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\View\View;
 
 class ListPosts extends ListRecords
 {
@@ -15,5 +17,19 @@ class ListPosts extends ListRecords
         return [
             CreateAction::make(),
         ];
+    }
+
+    public function getHeader(): ?View
+    {
+        $total = Post::count();
+        $published = Post::where('is_published', true)->count();
+        $drafts = Post::where('is_published', false)->count();
+
+        return view('filament.resources.posts.header', [
+            'total' => $total,
+            'published' => $published,
+            'drafts' => $drafts,
+            'createUrl' => PostResource::getUrl('create'),
+        ]);
     }
 }
