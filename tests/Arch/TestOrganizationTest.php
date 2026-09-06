73ab072dab1a95297d9736e84191dfac152835ec
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-06 04:03:26 -0400

    test: move newsletter rendering coverage to homepage

--- tests/Arch/TestOrganizationTest.php
+++ tests/Arch/TestOrganizationTest.php
@@ -46,7 +46,6 @@ test('test paths mirror their application source', function (string $suite): voi
         'Config/SentryTest.php' => 'config/sentry.php',
         'Views/AboutTest.php' => 'resources/views/about.blade.php',
         'Views/Components/Layouts/AppTest.php' => 'resources/views/components/layouts/app.blade.php',
-        'Views/Components/NewsletterProtectionTest.php' => 'resources/views/components/newsletter-protection.blade.php',
         'Views/Errors/Error404Test.php' => 'resources/views/errors/404.blade.php',
         'Views/Errors/Error419Test.php' => 'resources/views/errors/419.blade.php',
         'Views/Errors/Error500Test.php' => 'resources/views/errors/500.blade.php',
