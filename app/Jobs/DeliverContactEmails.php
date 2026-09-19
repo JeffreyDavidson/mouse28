16e95be6624cb2ca82fdd94321492975a88cc840
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-19 18:54:57 -0400

    chore: remove legacy contact email job
    * refactor: consolidate contact message email delivery
    
    * refactor: configure contact email job with queue attributes
    
    * refactor: use native contact job queue middleware
    
    * chore: remove seeded content cleanup command
    
    * chore: remove legacy contact email job

--- app/Jobs/DeliverContactEmails.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Jobs;
-
-/** Retained until queued and failed payloads using the previous class name are drained. */
-class DeliverContactEmails extends SendContactMessageEmails {}
